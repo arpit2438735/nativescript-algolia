9bad8d123ae21739fa1d07c01504fa71d87b4831
arpitsrivastava <asrivastava@aconex.com>
2017-05-22 13:07:34 +0530

    Chore:Scaffolding for nativescript-algolia search

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# nativescript-agolia
+# NativeScript-Algolia
+Nativescript plugin for Algolia(https://www.algolia.com/) search
+
+Fill in a little about your plugin!
+
+## License
+This plugin is licensed under the MITlicense by Arpit Srivastava
+
+## Installation
+To install type
+
+```
+tns plugin add nativescript-algolia
+```
+
+## Usages
+
+## Example
+
---
+++ index.android.ts
@@ -0,0 +1,13 @@
+/**********************************************************************************
+* (c) 2017, Arpit Srivastava.
+* Licensed under the MIT license.
+*
+* Version 1.0.0                                              arpit2438735@gmail.com
+**********************************************************************************/
+'use strict';
+
+export class Algolia { 
+  constructor() {
+    // Put in your initialization
+  }
+}
